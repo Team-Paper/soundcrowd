54dce7e26810073b8593cce81cee307fd2afd4d2
Tess Monical <tess.monical@gmail.com>
2017-09-27 14:55:21 -0500

    api routes tested using postman; they work :D

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/songs', require('./songs'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
