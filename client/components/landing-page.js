9797ea9f24d5e91a740ce8d55ddff831642d9444
Tess Monical <tess.monical@gmail.com>
2017-09-28 15:23:04 -0500

    landing page looks nicer!

--- client/components/landing-page.js
+++ client/components/landing-page.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
-import { fetchTopSongs } from '../store'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Item, Header } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { fetchTopSongs } from '../store';
 
 /**
  * COMPONENT
@@ -11,58 +12,68 @@ class LandingPage extends React.Component {
     if (this.props.fetchData) this.props.fetchData();
   }
 
-  componentWillReceiveProps (newProps) {
+  componentWillReceiveProps(newProps) {
     if (!this.props.fetchData && newProps.fetchData) {
       newProps.fetchData();
     }
   }
 
   render() {
-    if (!this.props.songs) return <div />
+    if (!this.props.songs) return <div />;
 
     return (
       <div>
-        {
-          this.props.songs.map(song => {
-            console.log("Current song is this: ", song)
-            return (
-              <div key={song.id}>
-                <h4>{song.title}</h4>
-                <audio controls>
-                  <source src={song.url} type="audio/mp3" />
-                </audio>
-              </div>
-            )
-          })
-        }
+        <Header>Top 50 Songs</Header>
+        <Item.Group relaxed>
+          {
+            this.props.songs.map((song, index) => {
+              return (
+                <Item key={song.id}>
+                  <Item.Image src={song.imageUrl || 'http://via.placeholder.com/150x150'} />
+                  <Item.Content>
+                    <Item.Header>
+                      <Header>#{index + 1}: {song.title}</Header>
+                    </Item.Header>
+                    <Item.Description>
+                      <br /><br />
+                      <audio controls>
+                        <source src={song.url} type="audio/mp3" />
+                      </audio>
+                    </Item.Description>
+                  </Item.Content>
+                </Item>
+              );
+            })
+          }
+        </Item.Group>
       </div>
-    )
+    );
   }
 }
 
 const mapState = (state) => {
   return {
-    //sort the songs and take the top 50 for this component
-    //we could have other songs in the state but we don't care about those right now
-    songs: state.songs.sort((songA, songB) => songB.playcount - songA.playcount).slice(0, 50)
-  }
-}
+    // sort the songs and take the top 50 for this component
+    // we could have other songs in the state but we don't care about those right now
+    songs: state.songs.sort((songA, songB) => songB.playcount - songA.playcount).slice(0, 50),
+  };
+};
 
 const mapDispatch = (dispatch) => {
   return {
     fetchData: () => {
-      //fetches the top 50 songs
+      // fetches the top 50 songs
       dispatch(fetchTopSongs(50));
-    }
-  }
-}
+    },
+  };
+};
 
-// /**
-//  * PROP TYPES
-//  */
-// LandingPage.propTypes = {
-//   fetchData: PropTypes.func.isRequired,
-//   songs: PropTypes.array.isRequired
-// }
+/**
+ * PROP TYPES
+ */
+LandingPage.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+  songs: PropTypes.array.isRequired,
+};
 
 export default connect(mapState, mapDispatch)(LandingPage);
