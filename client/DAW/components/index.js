56d7f62acb6b242b67b6a76b928b0c165af023f9
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-28 15:34:36 -0500

    Update props in timline and DAW component

--- client/DAW/components/index.js
+++ client/DAW/components/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Grid, Header, List, Segment } from 'semantic-ui-react';
+import { Grid, Header, List } from 'semantic-ui-react';
 import Timeline from './Timeline';
 import context from '../context';
 
@@ -9,7 +9,7 @@ import context from '../context';
  * COMPONENT
  */
 export const DAW = (props) => {
-  const { name, clips, library, tracks, settings } = props;
+  const { name, library, settings } = props;
 
   return (
     <Grid divided>
@@ -34,15 +34,8 @@ const mapState = () =>
   // return state.project
   ({
     name: 'Current Project',
-    clips: [
-      { url: '/GetToDaChoppa.mp3', startTime: 0, track: null },
-      { url: '/NotATumah.mp3', startTime: 0.5, track: 1 },
-    ],
     library: [
       { id: 1, url: '/NotATumah.mp3' },
-    ],
-    tracks: [
-      { id: 1, volume: 100, isMuted: false },
     ],
     settings: { tempo: 60, isMetronomeOn: false },
   });
@@ -55,9 +48,7 @@ export default connect(mapState)(DAW);
  */
 DAW.propTypes = {
   name: PropTypes.string.isRequired,
-  clips: PropTypes.arrayOf(PropTypes.object).isRequired,
   library: PropTypes.arrayOf(PropTypes.object).isRequired,
-  tracks: PropTypes.arrayOf(PropTypes.object).isRequired,
   settings: PropTypes.shape({
     tempo: PropTypes.number,
     isMetronomeOn: PropTypes.bool,
