33a3665401dfce600d283380f234ca0ad47f2cf7
Tess Monical <tess.monical@gmail.com>
2017-09-28 14:46:13 -0500

    adds landing page to the routes and component exports

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome} from './components'
+import {Main, Login, Signup, UserHome, LandingPage} from './components'
 import {me} from './store'
 
 /**
@@ -25,6 +25,8 @@ class Routes extends Component {
             {/* Routes placed here are available to all visitors */}
             <Route path='/login' component={Login} />
             <Route path='/signup' component={Signup} />
+            <Route exact path='/' component={LandingPage} />
+
             {
               isLoggedIn &&
                 <Switch>
