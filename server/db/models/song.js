3a1952f1b1e60d319983c44919d3d5ba6595744d
Tess Monical <tess.monical@gmail.com>
2017-09-29 10:17:46 -0500

    adds trackArtUrl and notes fields for a song

--- server/db/models/song.js
+++ server/db/models/song.js
@@ -1,33 +1,39 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const Song = db.define('song', {
   url: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+  },
+  trackArtUrl: {
+    type: Sequelize.STRING,
+  },
+  notes: {
+    type: Sequelize.TEXT,
   },
   length: Sequelize.FLOAT,
   playcount: {
     type: Sequelize.INTEGER,
-    defaultValue: 0
+    defaultValue: 0,
   },
   title: {
     type: Sequelize.STRING,
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
 
 Song.prototype.incrementPlaycount = function () {
   return this.update({ playcount: this.playcount + 1 });
-}
+};
 
 Song.prototype.like = function (userId) {
   return this.addUser(userId);
-}
+};
 
 Song.prototype.unlike = function (userId) {
   return this.removeUser(userId);
-}
+};
 
 module.exports = Song;
