81ba7fe4abf21a84756c3c664e6582a0438bec5b
Tess Monical <tess.monical@gmail.com>
2017-10-02 14:09:37 -0500

    style changes to make it nicer

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Input, Item, Grid, Image, Header, Label, Icon, Form, Button } from 'semantic-ui-react';
+import { Container, Item, Grid, Image, Header, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { fetchUserComments, fetchUserSongs } from '../store';
 
@@ -27,53 +27,50 @@ class UserHome extends React.Component {
     if (!user || !songs) return <div />;
 
     return (
-      <div>
-        <h3>Welcome, {user.email}</h3>
+      <Grid>
+        <Grid.Row columns={2}>
 
+          <Grid.Column width={4}>
+            <Image inline verticalAlign='top' src={user.userImage} size='medium' />
+            <Button icon='write' />
+          </Grid.Column>
 
-        <Grid>
-          <Grid.Row columns={2}>
+          <Grid.Column width={8}>
+            <Header dividing as='h3'>Welcome, {user.email}</Header>
+            <Header dividing as='h4'>Bio:</Header><Button icon='write' />
+            <Container text>{user.bio}</Container>
+          </Grid.Column>
 
-            <Grid.Column width={4}>
-              <Image inline verticalAlign='top' src={user.userImage} size='medium' />
-              <Button className='float-on-top'><Icon name='write' /></Button>
-            </Grid.Column>
+        </Grid.Row>
 
-            <Grid.Column width={8}>
-              <Label>Bio:</Label>
-              <div>{user.bio}</div>
-            </Grid.Column>
-
-          </Grid.Row>
-
-          <Grid.Row columns={1}>
-            <Grid.Column>
-              <Header>Your Songs:</Header>
-              <Item.Group>
-                {
-                  songs.map((song) => {
-                    return (
-                      <Item key={song.id} meta={null}>
-                        <Item.Image src={song.imageUrl || 'http://via.placeholder.com/150x150'} size='tiny' />
-                        <Item.Content>
-                          <Item.Header>
-                            <Header as='h4'><Link to={`/song/${song.id}`}>{song.title}</Link></Header>
-                          </Item.Header>
-                          <Item.Description>
-                            <audio controls>
-                              <source src={song.url} type="audio/mp3" />
-                            </audio>
-                          </Item.Description>
-                        </Item.Content>
-                      </Item>
-                    );
-                  })
-                }
-              </Item.Group>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <Header dividing>Your Posted Songs:</Header>
+            <Item.Group>
+              {
+                songs.map((song) => {
+                  return (
+                    <Item key={song.id} meta={null}>
+                      <Item.Image src={song.imageUrl || 'http://via.placeholder.com/150x150'} size='tiny' />
+                      <Item.Content>
+                        <Item.Header>
+                          <Header as='h4'><Link to={`/song/${song.id}`}>{song.title}</Link></Header>
+                        </Item.Header>
+                        <Item.Description>
+                          <audio controls>
+                            <source src={song.url} type="audio/mp3" />
+                          </audio>
+                          <Button compact negative icon="delete" />
+                        </Item.Description>
+                      </Item.Content>
+                    </Item>
+                  );
+                })
+              }
+            </Item.Group>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
