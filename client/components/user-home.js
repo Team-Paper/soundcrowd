38311184d3f8d5321236081646fc3d94552a9b59
Tess Monical <tess.monical@gmail.com>
2017-10-02 10:57:22 -0500

    made the linter happy

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,34 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 /**
  * COMPONENT
  */
 export const UserHome = (props) => {
-  const {email} = props
+  const { email } = props;
 
   return (
     <div>
       <h3>Welcome, {email}</h3>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
   return {
-    email: state.user.email
-  }
-}
+    email: state.user.email,
+    user: state.user,
+  };
+};
 
-export default connect(mapState)(UserHome)
+export default connect(mapState)(UserHome);
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
-  email: PropTypes.string
-}
+  email: PropTypes.string.isRequired,
+};
