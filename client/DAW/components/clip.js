504bfe7061840133d170b2c6a620d3e9be6fb321
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-10-02 21:51:48 -0500

    Reset drag offset on clip move

--- client/DAW/components/clip.js
+++ client/DAW/components/clip.js
@@ -18,29 +18,56 @@ const styles = {
 };
 
 
-const Clip = (props) => {
-  const { isDragging, baseClip, clip, zoom, updatePosition } = props;
-  return (
-    <Draggable
-      bounds=".track-list"
-      grid={[1, 154]}
-      onStop={updatePosition(baseClip, zoom)}
-    >
-      <div style={styles.clip(clip, zoom, isDragging)}>
-        {clip.url} starting at {clip.startTime}
-      </div>
-    </Draggable>
-  );
-};
+class Clip extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+    };
+
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+  }
+
+  handleDrag(e, data) {
+    this.setState({ x: data.x, y: data.y });
+  }
+
+  handleEnd(e, data) {
+    const { clip, zoom, updatePosition, baseClip } = this.props;
+    const newPosition = {
+      startTime: clip.startTime + (data.lastX / zoom),
+      // track: clip.track + +(data.lastY / 154),
+    };
+    this.setState({ x: 0, y: 0 });
+    updatePosition(baseClip, newPosition);
+  }
+
+  render() {
+    const { isDragging, clip, zoom } = this.props;
+    return (
+      <Draggable
+        bounds=".track-list"
+        grid={[1, 154]}
+        onDrag={this.handleDrag}
+        onStop={this.handleEnd}
+        position={this.state}
+      >
+        <div style={styles.clip(clip, zoom, isDragging)}>
+          {clip.url} starting at {clip.startTime}
+        </div>
+      </Draggable>
+    );
+  }
+}
 
 const mapState = (state, ownProps) => ({
   baseClip: state.clips[ownProps.clip.key],
 });
 
 const mapDispatch = (dispatch, ownProps) => ({
-  updatePosition: (clip, zoom) => (e, data) => {
-    // should also change track number >>> data.lastY / 154
-    const newPosition = { startTime: clip.startTime + (data.lastX / zoom) };
+  updatePosition: (clip, newPosition) => {
     const updatedClip = Object.assign({}, clip, newPosition);
     dispatch(updateClipThunk(ownProps.project, ownProps.clip.key, updatedClip));
   },
