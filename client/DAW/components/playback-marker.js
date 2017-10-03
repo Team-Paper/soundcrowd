88f1386d5d75b82171d0529e6dea8b5415d930b6
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-10-03 11:41:10 -0500

    Make playback marker draggable

--- client/DAW/components/playback-marker.js
+++ client/DAW/components/playback-marker.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Draggable from 'react-draggable';
 
 const styles = {
-  marker(time, zoom) {
-    return {
-      position: 'absolute',
-      top: '0',
-      left: '1px',
-      width: '1px',
-      height: '100%',
-      background: '#e22',
-      transform: `translateX(${time * zoom}px)`,
-    };
+  marker: {
+    position: 'absolute',
+    top: '0',
+    left: '1px',
+    width: '20px',
+    height: '100%',
+    background: '#e22',
   },
 };
 
-const PlaybackMarker = (props) => {
-  const { time, zoom } = props;
-  return (
-    <div style={styles.marker(time, zoom)} />
-  );
-};
+class PlaybackMarker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+    };
+
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({ x: newProps.time * newProps.zoom });
+  }
+
+  handleDrag(e, data) {
+    this.setState({ x: data.x });
+  }
+
+  handleEnd(e, data) {
+    // this.setState({ x: 0 });
+    console.log(data.lastX);
+  }
+
+  render() {
+    return (
+      <Draggable
+        axis="x"
+        bounds=".track-list"
+        onDrag={this.handleDrag}
+        onStop={this.handleEnd}
+        position={this.state}
+      >
+        <div style={styles.marker} />
+      </Draggable>
+    );
+  }
+}
 
 const mapState = state => ({
   time: state.timeline.time,
