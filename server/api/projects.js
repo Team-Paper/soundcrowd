eeac65c2145b9a52cd7632ee18941ab4e742e418
Tess Monical <tess.monical@gmail.com>
2017-10-03 11:13:03 -0500

    some gatekeepers aren't working the way I thought they were so they're removed for now and I'm only using ones I trust

--- server/api/projects.js
+++ server/api/projects.js
@@ -5,15 +5,15 @@ const { isSelfOrAdmin, isSelf } = require('./gatekeepers');
 module.exports = router;
 
 // get a specific project
-// requires that the logged in user is a collaborator or
-// that they are an admin
-router.get('/:id', isSelfOrAdmin, (req, res, next) => {
+router.get('/:projectId', (req, res, next) => {
   Project.findById(Number(req.params.id))
     .then(comment => res.json(comment))
     .catch(next);
 });
 
 // create a new project
+// the isSelf gatekeeper makes sure that users cannot
+// make a project for another user
 router.post('/', isSelf, (req, res, next) => {
   Project.create(req.body)
     .then(project => res.json(project))
