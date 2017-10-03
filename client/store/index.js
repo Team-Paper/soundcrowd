df5c61139f00b9ab5e0d7f3df642a0aaab1c8930
Tess Monical <tess.monical@gmail.com>
2017-10-03 12:07:38 -0500

    adds reducer for users so that we can keep other users (not just the logged-in user) in the store

--- client/store/index.js
+++ client/store/index.js
@@ -1,20 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import { createLogger } from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import user from './user'
-import songs from './songs'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import user from './user';
+import songs from './songs';
 import comments from './comments';
+import users from './users';
 
 const reducer = combineReducers({
   user,
   songs,
   comments,
-})
+  users,
+});
 
-const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-const store = createStore(reducer, middleware)
+const middleware = applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }));
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './user'
-export * from './songs'
+export default store;
+export * from './user';
+export * from './songs';
 export * from './comments';
+export * from './users';
