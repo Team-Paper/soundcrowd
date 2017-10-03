1f85ec961415e8006eb21ff0a639f85d977a46d7
Greg <ajaxgreater@gmail.com>
2017-10-03 17:29:34 -0500

    cleaned up console logs

--- server/auth/facebook.js
+++ server/auth/facebook.js
@@ -5,20 +5,6 @@ const {User} = require('../db/models')
 const axios = require('axios')
 module.exports = router
 
-/**
- * For OAuth keys and other secrets, your Node process will search
- * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
- * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - it should NOT be tracked
- * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment variables like so:
- *
- * process.env.GOOGLE_CLIENT_ID = 'your google client id'
- * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
- * process.env.GOOGLE_CALLBACK = '/your/google/callback'
- */
-
 
 
 const strategy = new FacebookStrategy({
@@ -41,10 +27,6 @@ function(req, accessToken, refreshToken, profile, done) {
     : User.create({name, email, facebookId})
       .then(user => done(null, user))
   )
-  // .then(() => axios.get(`https://graph.facebook.com/v2.9/${facebookId}?fields=id,name&accessToken=${accessToken}`)
-  // .then(response => {
-  //      console.log(response)
-  // }))
   .catch(done)
 
 }
@@ -61,8 +43,6 @@ router.get('/callback', passport.authenticate('facebook', {
 }))
 
 router.get('/friends', (req, res, next) => {
-  console.log('token', req.session.token)
-  console.log('id', req.session.facebookId)
   axios.get(`https://graph.facebook.com/v2.9/${req.session.facebookId}/friends?access_token=${req.session.token}`)
   .then(response => {
     res.json(response.data)
