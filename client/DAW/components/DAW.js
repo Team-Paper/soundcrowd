6da550c8478e0e36c4d58cdfac74aa40ae6e96db
Tess Monical <tess.monical@gmail.com>
2017-10-04 10:09:31 -0500

    we can list filenames!

--- client/DAW/components/DAW.js
+++ client/DAW/components/DAW.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Grid, Header, List } from 'semantic-ui-react';
 import { Timeline } from '../components';
@@ -9,14 +8,14 @@ import context from '../context';
  * COMPONENT
  */
 export const DAW = (props) => {
-  const { name, library, settings } = props;
+  const { name, files, settings } = props;
   return (
     <Grid divided padded>
       <Grid.Column width={3}>
         <Header as="h3">{name}</Header>
         <p>Tempo: {settings.tempo}</p>
         <List>
-          { library.map(item => <List.Item key={item.id}>{item.url}</List.Item>) }
+          { files.map(item => <List.Item key={item.id}>{item.url}</List.Item>) }
         </List>
       </Grid.Column>
       <Grid.Column width={13}>
@@ -33,23 +32,10 @@ const mapState = (state, ownProps) =>
   // return state.project
   ({
     name: 'Current Project',
-    library: [
-      { id: 1, url: '/NotATumah.mp3' },
-    ],
+    files: Object.entries(state.files).map(entry => entry[1]),
     settings: { tempo: 60, isMetronomeOn: false },
   });
 
 
 export default connect(mapState)(DAW);
 
-/**
- * PROP TYPES
- */
-DAW.propTypes = {
-  name: PropTypes.string.isRequired,
-  library: PropTypes.arrayOf(PropTypes.object).isRequired,
-  settings: PropTypes.shape({
-    tempo: PropTypes.number,
-    isMetronomeOn: PropTypes.bool,
-  }).isRequired,
-};
