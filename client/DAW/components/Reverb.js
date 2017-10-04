4ef362d9770fcbfa4eeaec2044154994b62d420b
Chris Skene <cnskene@gmail.com>
2017-10-04 16:57:23 -0500

    fix checkbox bug

--- client/DAW/components/Reverb.js
+++ client/DAW/components/Reverb.js
@@ -11,7 +11,7 @@ const ReverbModal = function({ projectId, track, setTrackReverbGain, toggleTrack
     <Modal.Header>Convolution Reverb (Track {track.id})</Modal.Header>
     <Modal.Content>
       <Modal.Description>
-        On: <Checkbox toggle checked={track.reverb.on ? 'true' : '' } onChange={() => toggleTrackReverb(projectId, track)} />
+        On: <Checkbox toggle checked={track.reverb.on ? true : false } onChange={() => toggleTrackReverb(projectId, track)} />
         <label>Gain: </label><input type="range" value={track.reverb.gain} onChange={e => setTrackReverbGain(projectId, track, e.target.value)} max={1} min={0} step={.01} />
         <Select placeholder="select reverb" options={reverbOptions} value={`${track.reverb.id}`} />
       </Modal.Description>
