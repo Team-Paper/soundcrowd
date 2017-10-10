2eae696d53cedd48c7aa8845b971900f16013ee5
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-10-10 14:46:28 -0500

    Fix sizing for svg tickmarks

--- client/DAW/components/tickmarks.js
+++ client/DAW/components/tickmarks.js
@@ -10,8 +10,8 @@ const TickMarks = (props) => {
   const { length, zoom } = props;
   const interval = 0.25;
 
-  const smallTickLength = 10;
-  const bigTickLength = 20;
+  const smallTickLength = 5;
+  const bigTickLength = 10;
 
   const svgElems = [];
   for (let i = 0; i <= length + 0.05; i += interval) {
@@ -21,12 +21,12 @@ const TickMarks = (props) => {
     svgElems.push(<line x1={startDistance.toString()} y1="0" x2={startDistance.toString()} y2={tickLength.toString()} key={`line${i}`} />);
 
     if (!(i % 1)) {
-      svgElems.push(<text textAnchor="middle" alignmentBaseline="hanging" x={startDistance.toString()} y="25" key={`text${i}`}>{i}</text>);
+      svgElems.push(<text fontSize="10" textAnchor="middle" alignmentBaseline="hanging" x={startDistance.toString()} y="15" key={`text${i}`}>{i}</text>);
     }
   }
 
-return (
-    <svg width="100%" height="25px" viewBox={"0 0 " + length * zoom + " 50"} style={style}>
+  return (
+    <svg width="100%" height="25px" viewBox={`0 0 ${length * zoom} 25`} style={style}>
       {svgElems}
     </svg>
   );
