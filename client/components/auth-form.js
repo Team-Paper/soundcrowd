52d94f3a266d686f118cea72a58f1698161a247f
Greg <ajaxgreater@gmail.com>
2017-10-10 13:39:29 -0500

    working on auth-form style

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Form, Container, Input, Button, Label } from 'semantic-ui-react';
+import { Form, Grid, Container, Header, Image, Message, Segment, Input, Button, Label } from 'semantic-ui-react';
 import { auth } from '../store';
 
 /**
@@ -11,27 +11,53 @@ const AuthForm = (props) => {
   const { name, displayName, handleSubmit, error } = props;
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit} name={name}>
-        <Form.Field>
-          <Label htmlFor='email'><small>Email</small></Label>
-          <Input name='email' type='text' />
-        </Form.Field>
-        <Form.Field>
-          <Label htmlFor='password'><small>Password</small></Label>
-          <Input name='password' type='password' />
-        </Form.Field>
-        <Form.Field>
-          <Container textAlign='center'>
-            <Button positive type='submit'>{displayName}</Button>
-          </Container>
-        </Form.Field>
-        {error && error.response && <div> {error.response.data} </div>}
-      </Form>
-      <Container textAlign='center'>
-        <a href='/auth/facebook'>{displayName} with Facebook</a>
-      </Container>
-    </Container>
+    <div className='login-form'>
+    {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+    <style>{`
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+    <Grid
+      textAlign='center'
+      style={{ height: '100%' }}
+      verticalAlign='middle'
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='grey' textAlign='center'>
+          {/* <Image src='/logo.png' /> */}
+          {''}{displayName}
+        </Header>
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon='user'
+              iconPosition='left'
+              placeholder='E-mail address'
+            />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+
+            <Button color='teal' fluid size='large'>Login</Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href='#'>Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </div>
   );
 };
 
@@ -82,3 +108,27 @@ AuthForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   error: PropTypes.object,
 };
+
+
+
+{/* <Container>
+<Form onSubmit={handleSubmit} name={name}>
+  <Form.Field>
+    <Label htmlFor='email'><small>Email</small></Label>
+    <Input name='email' type='text' />
+  </Form.Field>
+  <Form.Field>
+    <Label htmlFor='password'><small>Password</small></Label>
+    <Input name='password' type='password' />
+  </Form.Field>
+  <Form.Field>
+    <Container textAlign='center'>
+      <Button positive type='submit'>{displayName}</Button>
+    </Container>
+  </Form.Field>
+  {error && error.response && <div> {error.response.data} </div>}
+</Form>
+<Container textAlign='center'>
+  <a href='/auth/facebook'>{displayName} with Facebook</a>
+</Container>
+</Container> */}
